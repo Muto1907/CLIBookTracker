c05a0b4ac11a802731c344971651da3c52a4369d
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-01-02 00:29:26 +0100

    integrate store inside model

--- app/model.go
+++ app/model.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"log"
+
 	"github.com/Muto1907/CLIBookTracker/data"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -18,20 +20,26 @@ var docstyle = lipgloss.NewStyle().Margin(1, 2)
 type model struct {
 	books    []data.Book
 	state    uint
-	selected map[int]struct{}
+	store    *data.Store
 	list     list.Model
+	currBook data.Book
 }
 
-func NewModel() model {
-	books := []data.Book{
+func NewModel(store *data.Store) model {
+	/*books := []data.Book{
 		{Name: "Grokking Algorithms", Descr: "Algorithms and Datastructures", Pages: 43},
 		{Name: "Writing an Interpreter in Go", Descr: "Theoretical Computer Science", Pages: 32},
+	}*/
+	books, err := store.GetBooks()
+	if err != nil {
+		log.Fatalf("unable to get books: %v", err)
 	}
 	return model{
+		store:    store,
 		state:    listView,
 		books:    books,
-		selected: make(map[int]struct{}),
 		list:     list.New(data.BookToItems(books), list.NewDefaultDelegate(), 20, 14),
+		currBook: books[0],
 	}
 }
 
--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	"github.com/Muto1907/CLIBookTracker/app"
+	"github.com/Muto1907/CLIBookTracker/data"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(app.NewModel())
+	store := &data.Store{}
+	if err := store.Init(); err != nil {
+		log.Fatalf("Unable to init store: %v", err)
+	}
+
+	p := tea.NewProgram(app.NewModel(store))
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+		log.Fatalf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
 }
